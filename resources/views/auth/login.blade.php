8b4c2d7bf6fdce22145beecd1eb38135d4d6d7db
vishwajitvm <vishwajitmall50@gmail.com>
2022-05-22 13:16:24 +0530

    login page setup is completed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 <!-- Mirrored from html.designstream.co.in/pick/html/page-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 21 May 2022 18:56:06 GMT -->
 <head>
         <meta charset="UTF-8">
-        <title>Pick Admin</title>
+        <title> {{ env('APP_NAME') }} </title>
         <link rel="shortcut icon" href="{{asset('backend/dist/images/favicon.ico')}}" />
         <meta name="viewport" content="width=device-width,initial-scale=1"> 
 
@@ -34,43 +34,59 @@
         <div class="container">
             <div class="row vh-100 justify-content-between align-items-center">
                 <div class="col-12">
-                    <form action="#" class="row row-eq-height lockscreen  mt-5 mb-5">
+                    <x-jet-validation-errors class="mb-4" />
+
+                    @if (session('status'))
+                        <div class="mb-4 font-medium text-sm text-green-600">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('login') }}" class="row row-eq-height lockscreen  mt-5 mb-5">
+                        @csrf
                         <div class="lock-image col-12 col-sm-5"></div>
                         <div class="login-form col-12 col-sm-7">
                             <div class="form-group mb-3">
                                 <label for="emailaddress">Email address</label>
-                                <input class="form-control" type="email" id="emailaddress" required="" placeholder="Enter your email">
+                                <input class="form-control"  id="email" type="email" name="email" :value="old('email')" required autofocus  placeholder="Enter your email">
                             </div>
 
                             <div class="form-group mb-3">
                                 <label for="password">Password</label>
-                                <input class="form-control" type="password" required="" id="password" placeholder="Enter your password">
+                                <input class="form-control" type="password" id="password" placeholder="Enter your password"  name="password" required autocomplete="current-password">
                             </div>
 
-                            <div class="form-group mb-3">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="checkbox-signin" checked="">
-                                    <label class="custom-control-label" for="checkbox-signin">Remember me</label>
+                            <div class="form-group mb-3 ">
+                                <div class="row">
+                                    <div class="col-5 custom-control custom-checkbox display-inline">
+                                        <input type="checkbox" class="custom-control-input"  id="remember_me" name="remember" >
+                                        <label class="custom-control-label" for="remember_me">Remember me</label>
+                                    </div>
+    
+                                    <div class="col-7 flex items-center justify-end ">
+                                        @if (Route::has('password.request'))
+                                            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                                                {{ __('Forgot your password?') }}
+                                            </a>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
 
+                            
+
                             <div class="form-group mb-0">
-                                <button class="btn btn-primary" type="submit"> Log In </button>
+                                <button class="btn btn-primary btn-block" type="submit"> Log In </button>
                             </div>
-                            <p class="my-2 text-muted">--- Or connect with ---</p>
-                            <a class="btn btn-social btn-dropbox text-white mb-2">
-                                <i class="icon-social-dropbox align-middle"></i>
-                            </a>
-                            <a class="btn btn-social btn-facebook text-white mb-2">
-                                <i class="icon-social-facebook align-middle"></i>
-                            </a>                                   
-                            <a class="btn btn-social btn-github text-white mb-2">
-                                <i class="icon-social-github align-middle"></i>
-                            </a>
-                            <a class="btn btn-social btn-google text-white mb-2">
+                            <p class="my-2 text-muted">--- Or Login with Google ---</p>
+                        
+                            <a href="{{ Route('auth.google') }}" class="btn btn-social btn-google text-white mb-2 btn-block">
                                 <i class="icon-social-google align-middle"></i>
                             </a>
                             <div class="mt-2">Don't have an account? <a href="page-register.html">Create an Account</a></div>
+
+                           
+
                         </div>
                     </form>
                 </div>
